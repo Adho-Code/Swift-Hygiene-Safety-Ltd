f22be062431080b945c38dbb32b261bea9ea9d5b
HabeshaQueen <adhoadhigal.com>
2020-03-05 12:59:14 +0300

    add changes

---
+++ sending_email/email.js
@@ -0,0 +1,30 @@
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'adhoadhigal@gmail.com',
+    pass: '38487764'
+  }
+});
+
+
+
+
+module.exports = function sendMail(email, subject, text) {
+    var mailOptions = {
+        from: 'adhoadhigal@gmail.com',
+        to: email,
+        subject: subject,
+        text: text
+    }
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
+
+}
