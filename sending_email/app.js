e1f9e30b8d47bd81b3a180a54d805b5994c089e0
HabeshaQueen <adhoadhigal.com>
2020-03-06 11:46:35 +0300

    add emails validation

--- sending_email/app.js
+++ sending_email/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const sendMail = require('./email');
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
@@ -10,7 +12,13 @@ app.get('/', (req, res) => res.send('hello world') )
 
 app.post('/request-service', (req, res) => {
     console.log(req.body);
-    let text = `${req.body.firstname} requested a service`;
+    let text = `Hi,
+    ${req.body.firstname} ${req.body.lastname} ,
+    has booked ${req.body.labour} on,
+     ${req.body.date} for ${req.body.duration},
+      ${req.body.contact},
+      ${req.body.location_id}`
+    // let text = `${req.body.firstname} ${req.body.lastname} ${req.body.username} ${req.body.email} ${req.body.labour} ${req.body.duration} ${req.body.contact} ${req.body.location_id} requested a service`;
     sendMail('adhoadhigal@gmail.com','Service Request', text);
     res.send('success full');
 } )
